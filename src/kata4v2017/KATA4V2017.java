45ff8c9ae56e46fcf40e36f86550808d5e91de26
Enrique José Ramón Balmaseda <ejrbalma@gmail.com>
2017-10-29 16:41:25 +0000

    UPDATE se han elimando println.

--- src/kata4v2017/KATA4V2017.java
+++ src/kata4v2017/KATA4V2017.java
@@ -6,13 +6,11 @@ import java.util.ArrayList;
 public class KATA4V2017 {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Inicio");
         String nameFile = "/Users/ejrbalma/Google Drive/Curso 2017 2018/ISII/KATA/Data/emailsfile.txt";
         ArrayList <String> arrayMail = MailList.readFile(nameFile);
         Histogram<String> histogram = MailHistogramBuilder.build(arrayMail);
         new HistogramDisplay(histogram).execute();
 
-        System.out.println("Fin");
         
     }
     
